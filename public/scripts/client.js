fb3d6b6b5704c8f9ee5c5ba80093fdea29539171
Mitchell Aldrich <mitch.w.aldrich@gmail.com>
2021-07-15 00:47:22 +0000

    implemented function createTweetElement in client.js and tested that it rendered in the browser

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,66 @@
 /*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+* Client-side JS logic goes here
+* jQuery is already loaded
+* Reminder: Use (and do all your DOM work in) jQuery's document ready function
+*/
+let tweetExample = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
+
+$(document).ready(function() {
+  console.log('client.js running')
+  const createTweetElement = function(tweetObj) {
+    const { user, content, created_at } = tweetObj;
+    const { name, avatars, handle } = user;
+    const { text } = content;
+    
+    const $tweet = $(`<article class=tweet>
+    <header>
+      <div class=user>
+      <img class="avatar" src="${avatars}">
+      <h4>${name}</h4>
+      </div>
+      <h4 id="handle">${handle}</h4>
+    </header>
+    <h5>${text}</h5>
+    <footer>
+      <p>${created_at}</p>
+      <div id="icons">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </div>
+    </footer>
+  </article>`)
+
+    return $tweet;
+  }
+
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+    "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+    "created_at": 1461116232227
+ }
+
+const $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+$('#tweets-container').append($tweet); 
+
+});
 
