4ccb65d233de8773b4960d33ccfcbe8310148dd0
Mitchell Aldrich <mitch.w.aldrich@gmail.com>
2021-07-16 04:47:51 +0000

    linted composer-char-counter.js

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
-  console.log('char-counter is ready')
+  console.log('char-counter is ready');
   $('#tweet-text').on('input', function() {
     let countdown = (140 - this.value.length);
-    console.log('char', this.value.length)
     if (countdown < 0) {
       $('output').css('color', 'red');
     }
+    if (countdown >= 0) {
+      $('output').css('color', '#545149');
+    }
     $('output').html(`${countdown}`);
-  })
-    // console.log(140 - this.value.length);
+  });
 });
